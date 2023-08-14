5230305c872ede7e10201653884f73242877f008
Jay <54296281@qq.com>
2023-08-14 23:02:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import torch
 import sys
+import gradio as gr
 
 model_name = sys.argv[1]
+
+
+def no_model(name):
+    return "Sorry, the model name you input is wrong!"
+
+
+def no_cpu(name):
+    return "Sorry, the demo can not run on cpu!"
+
+
 if torch.cuda.is_available():
     if model_name == 'llama':
         from llama_2_7b import demo
 
-        demo.queue().launch(server_name="0.0.0.0")
     elif model_name == 'stable_diffusion':
         from sdxl_base_1 import demo
 
-        demo.launch(server_name="0.0.0.0")
     else:
-        print('nothing!')
-
+        demo = gr.Interface(fn=no_model, inputs="text", outputs="text")
 else:
-    pass
+    demo = gr.Interface(fn=no_cpu, inputs="text", outputs="text")
+
+demo.launch(server_name="0.0.0.0")
+
