c6f3ed212aefbc80eecc5da5d7740325458e8c07
Jay <54296281@qq.com>
2023-08-14 22:30:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ if torch.cuda.is_available():
     if model_name == 'llama':
         from llama_2_7b import demo
 
-        demo.launch(server_name="0.0.0.0")
+        demo.queue().launch(server_name="0.0.0.0")
     elif model_name == 'stable_diffusion':
         from sdxl_base_1 import demo
 
-        demo.queue().launch(server_name="0.0.0.0")
+        demo.launch(server_name="0.0.0.0")
     else:
         print('nothing!')
 
