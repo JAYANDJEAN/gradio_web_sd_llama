bcf7d3e577ff74e3b1a9cc85b96f298bdcf816f9
Jay <54296281@qq.com>
2023-08-14 20:36:34 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import torch
+
+model_name = sys.argv[1]
+if torch.cuda.is_available():
+    if model_name == 'llama':
+        from llama_2_7b import demo
+
+        demo.launch(server_name="0.0.0.0")
+    elif model_name == 'stable_diffusion':
+        from sdxl_base_1 import demo
+
+        demo.launch(server_name="0.0.0.0")
+    else:
+        print('nothing!')
+
+else:
+    pass
